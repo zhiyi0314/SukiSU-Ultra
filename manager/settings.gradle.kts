6139d1da6ae362eebc8b79a3182829c45eca7a66
zhiyi <15512325974@163.com>
2025-10-07 00:40:35 +0800

    Update settings.gradle.kts

--- manager/settings.gradle.kts
+++ manager/settings.gradle.kts
@@ -6,16 +6,17 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven("https://jitpack.io")
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
-rootProject.name = "KernelSU"
+rootProject.name = "MiuiXSukiManager"
 include(":app")
